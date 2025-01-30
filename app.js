b3858204a64b346c17b82d3708599be6f4b88dfe
jesus <110626371+malitokira99@users.noreply.github.com>
2025-01-29 20:39:27 -0500

    // Content:modificado app.js variable de entorno para puerto

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/',(req, res) => {
 
 
 //iniciar servidor 
-
-app.listen(3000, () => {
+const PORT =process.env.PORT ?? 3000;
+app.listen(PORT, () => {
     console.log('servidor corriendo en el puerto http://localhost:3000');
 });
